9f3f29dd38d19ddb896c3a72ee4b89f68dec6016
Shark'ur <94655227+Shaarkzy@users.noreply.github.com>
2024-11-17 17:51:36 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,12 +7,12 @@ if [ "$user_input" -eq 1 ]; then
     echo 'INSTALLING PYTHON3'
     pkg install python3
     echo 'DONE'
-    ./UTILS/android_setup.py
+    ./UTILS/android_setup.sh
 elif [ "$user_input" -eq 2 ]; then
     echo 'INSTALLING PYTHON3'
     apt-get install python3
     echo 'DONE'
-    ./UTILS/kali_setup.py
+    ./UTILS/kali_setup.sh
 else
     echo 'INVALID INPUT'
 fi
